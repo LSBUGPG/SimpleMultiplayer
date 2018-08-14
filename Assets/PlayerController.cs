7b382637a7ee548ca00646ca475045cb25c35920
Paul Sinnett <paul.sinnett@gmail.com>
2018-08-14 14:50:07 +0100

    add shootas

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -3,6 +3,9 @@ using UnityEngine.Networking;
 
 public class PlayerController : NetworkBehaviour
 {
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
+
     public override void OnStartLocalPlayer()
     {
         GetComponent<Renderer>().material.color = Color.blue;
@@ -20,5 +23,25 @@ public class PlayerController : NetworkBehaviour
 
         transform.Rotate(0, x, 0);
         transform.Translate(0, 0, z);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Fire();
+        }
     }
+    
+    void Fire()
+    {
+        // Create the Bullet from the Bullet Prefab
+        var bullet = (GameObject)Instantiate (
+            bulletPrefab,
+            bulletSpawn.position,
+            bulletSpawn.rotation);
+
+        // Add velocity to the bullet
+        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 6;
+
+        // Destroy the bullet after 2 seconds
+        Destroy(bullet, 2.0f);
+    }    
 }
