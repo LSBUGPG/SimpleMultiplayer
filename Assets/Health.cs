e4f4cb2fc13bba299e27805d76da1d0034092a9d
Paul Sinnett <paul.sinnett@gmail.com>
2018-08-14 15:39:32 +0100

    synchronise health loss

--- Assets/Health.cs
+++ Assets/Health.cs
@@ -2,20 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Networking;
 
-public class Health : MonoBehaviour {
+public class Health : NetworkBehaviour {
 
 	public const int maxHealth = 100;
+	[SyncVar(hook = "OnChangeHealth")]
 	public int currentHealth = 100;
 	public RectTransform healthBar;
 	public void TakeDamage(int amount)
 	{
+		if (!isServer)
+		{
+			return;
+		}
 		currentHealth -= amount;
-		healthBar.sizeDelta = new Vector2(currentHealth, healthBar.sizeDelta.y);
 		if (currentHealth <= 0)
 		{
 			currentHealth = 0;
 			Debug.Log("Dead!");
 		}
+	}
+	void OnChangeHealth (int health)
+	{
+		healthBar.sizeDelta = new Vector2(health, healthBar.sizeDelta.y);
 	}
 }
